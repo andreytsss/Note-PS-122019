7228ca8209939cbc72e386bf0d3e963fbd36a480
andreytsss <themarcocraft@mail.ru>
2021-09-28 19:21:38 +0300

    note

---
+++ Note-PS-122019/Note-PS-122019/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Note_PS_122019.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
